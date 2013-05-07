f37632574c34694c9071c8cc181e2ac7ce7fb7fa
tjark1989 <drittewahl_89@web.de>
2013-05-07 12:00:43 +0200

    TestMethods in StudentLogic

--- CourseManagement/Client/BusinessLogic/StudentLogic.cs
+++ CourseManagement/Client/BusinessLogic/StudentLogic.cs
@@ -11,16 +11,36 @@ namespace CourseManagement.Client.BusinessLogic
 {
     public static class StudentLogic
     {
-        public static DataSet getAll()
+        public static DataSet getDataSet()
         {
             DataSet studentSet = new DataSet();
             DataTable studentTable = new DataTable();
-            foreach (PropertyInfo pi in (new Student()).GetType().GetProperties())
+            studentTable.Columns.Add("Test");
+            studentTable.Rows.Add("TestWert");
+            studentTable.Rows.Add("Testwert2");
+            studentSet.Tables.Add(studentTable);
+            /*foreach (PropertyInfo pi in (new Student()).GetType().GetProperties())
             {
                 studentTable.Columns.Add(pi.Name);
             }
-            studentSet.Tables.Add(studentTable);
+            studentSet.Tables.Add(studentTable);*/
             return studentSet;
+        }
+
+        public static DataTable getDataTable()
+        {
+            
+            DataTable studentTable = new DataTable();
+            studentTable.Columns.Add("Test");
+            studentTable.Rows.Add("TestWert");
+            studentTable.Rows.Add("Testwert2");
+           
+            /*foreach (PropertyInfo pi in (new Student()).GetType().GetProperties())
+            {
+                studentTable.Columns.Add(pi.Name);
+            }
+            studentSet.Tables.Add(studentTable);*/
+            return studentTable;
         }
     }
 }
