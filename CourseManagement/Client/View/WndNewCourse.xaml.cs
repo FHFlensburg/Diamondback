9d3dc920e4e345a5318852ea7fb5b9b48bbf4c5a
FHFlensburg <andre.schiemann@stud.fh-flensburg.de>
2013-05-31 00:33:05 +0200

    New Course

--- CourseManagement/Client/View/WndNewCourse.xaml.cs
+++ CourseManagement/Client/View/WndNewCourse.xaml.cs
@@ -31,18 +31,53 @@ namespace CourseManagement.Client.View
 
         private void setGuiValues()
         {
-//            cboxitem = new ComboBoxItem();
-//cboxitem.Content = "Created with C#";
-//cbox.Items.Add(cboxitem);
-            cbMaxParticipants.Background = Brushes.LightBlue;
-            ComboBoxItem foo = new ComboBoxItem();
-            foo.Content = 1;
-            cbMaxParticipants.Items.Add(foo);
+            // 0 Participants allowed in Comboboxes
+            cbMaxParticipants.Items.Clear();
+            cbMinParticipants.Items.Clear();
+
+            // Values for Tutor
+            DataTable tutors = null;
+            tutors = TutorLogic.getInstance().getAll();
+            string temporaryTutor = string.Empty;
+            int countTutors = tutors.Rows.Count;
+
+            // Values for RoomNumber
+            DataTable roomNumber = null;
+            roomNumber = RoomLogic.getInstance().getAll();
+            string temporaryRoomNumber = string.Empty;
+            int countRoomNumbers = roomNumber.Rows.Count;
+
+            // Participants
+            for (int i = 0; i < 51; i++)
+            {
+                ComboBoxItem maximalParticipants = new ComboBoxItem();
+                ComboBoxItem minimalParticipants = new ComboBoxItem();
+                maximalParticipants.Content = i;
+                minimalParticipants.Content = i;
+                cbMaxParticipants.Items.Add(maximalParticipants);
+                cbMinParticipants.Items.Add(minimalParticipants);
+            }
+
+            // Tutors
+            for (int i = 0; i < countTutors; i++)
+            {
+                temporaryTutor = string.Empty;
+                temporaryTutor = tutors.Rows[i]["Surname"].ToString();
+                cbTutor.Items.Add(temporaryTutor);
+            }
+
+            // RoomNumber
+            for (int i = 0; i < countRoomNumbers; i++)
+            {
+                temporaryRoomNumber = string.Empty;
+                temporaryRoomNumber = roomNumber.Rows[i]["RoomNr"].ToString();
+                cbRoomNumber.Items.Add(temporaryRoomNumber);
+            }
+    
         }
 
         private void btnSave_Click(object sender, RoutedEventArgs e)
         {
-            CourseLogic createCourse = CourseLogic.getInstance();
             insertNewCourseAndValidate();
             this.Close();
         }
@@ -54,7 +89,11 @@ namespace CourseManagement.Client.View
             string costs = tbCosts.Text;
             string validInMonths = tbValidInMonth.Text;
             string maxParticipants = cbMaxParticipants.Text;
-            MessageBox.Show(maxParticipants);
+            string minParticipants = cbMinParticipants.Text;
+            string tutor = cbTutor.Text;
+            string room = cbRoomNumber.Text;
+            CourseLogic course = CourseLogic.getInstance();
+            //course.create();
             
         }     
     }
